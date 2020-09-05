95de6517fe2655d6b803c099c6f39803eb4edfdb
Haneen Amro <heno.amro@gmail.com>
2020-09-05 12:31:57 -0700

    pushed

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,19 +2,47 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const messageQueue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
-let messageQueue = null;
-module.exports.initialize = (queue) => {
-  messageQueue = queue;
+// var messageQueue = messageQueue;
+// module.exports.initialize = (queue) => {
+//   messageQueue = queue;
+// };
+//enqueue?
+const commands = ['up', 'down', 'left', 'right'];
+module.exports.router = (req, res, next = ()=>{}) => {
+  // console.node('Serving request type ' + req.method + ' for url ' + req.url);node .
+  console.log('hello from server');
+
+  if (req.method === 'OPTIONS' && req.url === '/') {
+    res.writeHead(200, headers);
+    res.end();
+    next();
+  }
+
+  if (req.method === 'GET' && req.url === '/') {
+    console.log(messageQueue.messages);
+    let random = Math.floor(Math.random() * commands.length);
+    res.writeHead(200, headers);
+    res.end(messageQueue.dequeue());
+    // res.end(commands[random]);
+
+    next();
+
+  }
+  //if req.method === post return data
+// post the background img
+// invoke next() at the end of a request to help with testing!
+  if (req.method === 'POST' && req.url === '/') {
+    //if the imag is missing
+    res.writeHead(404, headers);
+    res.end();
+    next();
+
+  }
 };
 
-module.exports.router = (req, res, next = ()=>{}) => {
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
-};
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -22,13 +22,18 @@ describe('server responses', () => {
   });
 
   it('should respond to a GET request for a swim command', (done) => {
-    // write your test here
+    let {req, res} = server.mock('/', 'GET');
+    httpHandler.router(req, res);
+    expect(res._responseCode).to.equal(200);
+    expect(res._ended).to.equal(true);
+    expect(res._data.toString()).to.not.equal(undefined);
+
     done();
   });
 
   xit('should respond with 404 to a GET request for a missing background image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock('FILL_ME_IN', 'GET');
+    let {req, res} = server.mock('/', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
@@ -47,7 +52,7 @@ describe('server responses', () => {
   xit('should respond to a POST request to save a background image', (done) => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
-      let {req, res} = server.mock('FILL_ME_IN', 'POST', fileData);
+      let {req, res} = server.mock('/', 'POST', fileData);
 
       httpHandler.router(req, res, () => {
         expect(res._responseCode).to.equal(201);
@@ -60,10 +65,10 @@ describe('server responses', () => {
   xit('should send back the previously saved image', (done) => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
-      let post = server.mock('FILL_ME_IN', 'POST', fileData);
+      let post = server.mock('/', 'POST', fileData);
 
       httpHandler.router(post.req, post.res, () => {
-        let get = server.mock('FILL_ME_IN', 'GET');
+        let get = server.mock('/', 'GET');
         httpHandler.router(get.req, get.res, () => {
           expect(Buffer.compare(fileData, get.res._data)).to.equal(0);
           done();
